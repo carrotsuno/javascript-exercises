2418f815d84bce4937170f0d085fb80b3895205c
Carrotsu <91496389+carrotsuno@users.noreply.github.com>
2024-08-16 15:17:00 -0700

    complete exercise 02_repeatString
    passes all test cases

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,14 @@
-const repeatString = function() {
-
+const repeatString = function(stringToRepeat, numberOfTimes) {
+    if (numberOfTimes < 0) {
+        return 'ERROR';
+    }
+    else {
+        let completedString = '';
+        for (i = 1; i <= numberOfTimes; i++) {
+            completedString = completedString + stringToRepeat;
+        }
+        return completedString;
+    }
 };
 
 // Do not edit below this line
