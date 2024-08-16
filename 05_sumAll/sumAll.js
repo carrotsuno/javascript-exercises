813db79eca74ce2fd87f9513ca069957eecda34d
Carrotsu <91496389+carrotsuno@users.noreply.github.com>
2024-08-16 16:30:28 -0700

    Complete exercise 05_sumAll
    Pass all test cases

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,25 @@
-const sumAll = function() {
-
+const sumAll = function(num1, num2) {
+    let finalSum = 0;
+    if(num1 < 0 
+        || num2 < 0
+        || typeof num1 !== "number" 
+        || typeof num2 !== "number" 
+        || num1 - Math.floor(num1) !== 0 
+        || num2 - Math.floor(num2) !== 0) {
+        return 'ERROR';
+    }
+    if (num1 < num2) {
+        for (i = num1; i <= num2; i++) {
+            finalSum = finalSum+i;
+        }
+    }
+    else {
+        for (i = num2; i <= num1; i++) {
+            finalSum = finalSum+i;
+        }
+    }
+    console.log(finalSum)
+    return finalSum;
 };
 
 // Do not edit below this line
