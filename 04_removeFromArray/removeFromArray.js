651eaa6c228e2ffab0c1da939a98e5495b9d4163
Carrotsu <91496389+carrotsuno@users.noreply.github.com>
2024-08-16 16:07:07 -0700

    Completed exercise 04_removeFromArray
    Pass all test cases

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,16 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, ...args) {
+    console.log('Array before: ' + array)
+    for (i = 0; i <= array.length - 1; i++) {
+        for (j = 0; j <= args.length - 1; j++) {
+            if (array[i] === args[j]) {
+                console.log('Match found: ' + array[i]);
+                array.splice(i,1);
+                i--;
+            }
+        }
+    }
+    console.log('Array after ' + array);
+    return array;
 };
 
 // Do not edit below this line
