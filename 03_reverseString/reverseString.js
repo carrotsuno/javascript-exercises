4300482492d76110443ab7fd86250725b00a8a09
Carrotsu <91496389+carrotsuno@users.noreply.github.com>
2024-08-16 15:36:23 -0700

    Complete exercise 03_reverseString
    All test cases passed

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,13 @@
-const reverseString = function() {
+const reverseString = function(stringToReverse) {
+    let arrayOfChars = stringToReverse.split('');
+    let reversedArray = [];
 
+    for (i = arrayOfChars.length - 1; i >= 0; i--) {
+        reversedArray.push(arrayOfChars[i]);
+    }
+
+    let reversedString = reversedArray.join('');
+    return reversedString;
 };
-
 // Do not edit below this line
 module.exports = reverseString;
