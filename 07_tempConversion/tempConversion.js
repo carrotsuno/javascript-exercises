95a8cb6117b03c672ebb31a86c2587ccde0a0edc
Carrotsu <91496389+carrotsuno@users.noreply.github.com>
2024-08-20 14:17:37 -0700

    Complete exercise 07_tempConversion
    Solution passes all test cases

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,24 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(tempInF) {
+    const tempInC = (tempInF - 32) * (5 / 9);
+    const rounded = Math.round(tempInC * 10) / 10;
+
+    if (tempInC - Math.floor(tempInC) == 0) {
+      return tempInC;
+    }
+    else{
+      return rounded;
+    }
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(tempInC) {
+    const tempInF = tempInC * (9 / 5) + 32;
+    const rounded = Math.round(tempInF * 10) / 10;
+    if (tempInF - Math.floor(tempInF) == 0) {
+      return tempInF;
+    }
+    else{
+      return rounded;
+    }
 };
 
 // Do not edit below this line
